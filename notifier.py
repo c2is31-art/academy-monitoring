1f55453cdbbeecde0f98722722b8e5cf462c6a54
c2is31-art <c2is31@gmail.com>
2026-09-23 17:23:09 +0900

    Add files via upload

--- notifier.py
+++ notifier.py
@@ -14,7 +14,10 @@ SENDER_PASSWORD = "us9wwst7vhqyxxe"     # Dooray 비밀번호 / 앱 비밀번호
 # 📧 보고서를 받을 이메일 목록
 RECEIVER_EMAILS = [
        "c2is@megastudy.net",
-       
+       "profilm@megastudyedu.com",
+    "tocka@megastudyedu.com",
+    "kjm99@megastudyedu.com",
+"hbkim@megastudyedu.com"
 ]
 
 def format_item_to_html(item_text):
